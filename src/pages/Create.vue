89de08f3a470e742d402fcfe6d32c1a285c6665e
npwolf <dev@wolfplusplus.com>
2022-04-03 09:29:14 -0700

    create layout

--- src/pages/Create.vue
+++ src/pages/Create.vue
@@ -5,76 +5,102 @@
       class="q-pa-md col"
       style="max-width: 800px"
     >
-      <div>
-        <q-form class="q-gutter-y-md">
-          <q-input
-            filled
-            v-model="name"
-            label="Name of Hike"
-            placeholder="Xyz Trail"
-          />
-          <q-input
-            filled
-            v-model="distance"
-            label="Distance in Miles"
-            placeholder="5.5"
-          />
-          <q-input
-            filled
-            v-model="gain"
-            label="Gain in Feet"
-            placeholder="550"
-          />
-          <q-input
-            filled
-            v-model="meetDate"
-            mask="date"
-            label="Meet Date"
-            :hint="largeMeetDate"
-            :rules="['date']"
-            style="max-width: 150px"
-          >
-            <template v-slot:append>
-              <q-icon
-                name="event"
-                class="cursor-pointer"
-              >
-                <q-popup-proxy
-                  ref="qDateProxy"
-                  cover
-                  transition-show="scale"
-                  transition-hide="scale"
+      <q-form class="q-gutter-y-md">
+
+        <div class="row">
+          <div class="col-grow">
+            <q-input
+              filled
+              v-model="name"
+              label="Name of Hike"
+              placeholder="Xyz Trail"
+            />
+          </div>
+        </div>
+
+        <div class="row q-gutter-y-md q-col-gutter-md">
+          <!-- <div class="row q-gutter-y-md q-col-gutter-md"> -->
+          <div class="col-12 col-sm-3">
+            <q-input
+              filled
+              v-model="distance"
+              label="Distance in Miles"
+              placeholder="5.5"
+            />
+          </div>
+          <div class="col-12 col-sm-3">
+            <q-input
+              filled
+              v-model="gain"
+              label="Gain in Feet"
+              placeholder="550"
+            />
+          </div>
+          <div class="col-12 col-sm-3">
+            <q-input
+              filled
+              v-model="meetDate"
+              mask="date"
+              label="Meet Date"
+              :hint="largeMeetDate"
+              :rules="['date']"
+            >
+              <template v-slot:append>
+                <q-icon
+                  name="event"
+                  class="cursor-pointer"
                 >
-                  <q-date v-model="meetDate">
-                    <div class="row items-center justify-end">
-                      <q-btn
-                        v-close-popup
-                        label="Close"
-                        color="primary"
-                        flat
-                      />
-                    </div>
-                  </q-date>
-                </q-popup-proxy>
-              </q-icon>
-            </template>
-          </q-input>
+                  <q-popup-proxy
+                    ref="qDateProxy"
+                    cover
+                    transition-show="scale"
+                    transition-hide="scale"
+                  >
+                    <q-date v-model="meetDate">
+                      <div class="row items-center justify-end">
+                        <q-btn
+                          v-close-popup
+                          label="Close"
+                          color="primary"
+                          flat
+                        />
+                      </div>
+                    </q-date>
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+            </q-input>
+          </div>
+          <div class="col-12 col-sm-3">
+            <q-input
+              v-model="meetTime"
+              filled
+              type="time"
+              label="Meet Time"
+            />
+          </div>
+        </div>
 
-          <q-input
-            v-model="meetTime"
-            filled
-            type="time"
-            label="Meet Time"
-            style="max-width: 150px"
-          />
+        <div class="row">
+          <div class="col-grow">
+            <q-input
+              filled
+              v-model="name"
+              label="Link to Hike"
+              placeholder="https://www.alltrails.com/trail/..."
+            />
+          </div>
+        </div>
 
+        <div class="row justify-end">
           <q-btn
             color="secondary"
             @click.prevent="createEvent"
           >Create</q-btn>
-        </q-form>
-      </div>
+        </div>
+      </q-form>
     </div>
+
     <div class="col-shrink">&nbsp;</div>
   </q-page>
 </template>
@@ -104,7 +130,7 @@ export default defineComponent({
   },
   computed: {
     largeMeetDate () {
-      return moment(this.meetDate).format("dddd, MMMM Do YYYY") + " - " + moment(this.meetDate).fromNow();
+      return moment(this.meetDate).format("ddd MMM Do YYYY") + " - " + moment(this.meetDate).fromNow();
     },
   },
 });
